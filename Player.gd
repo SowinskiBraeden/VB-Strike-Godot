60b21ea5395d71cf7cb4968cea46a8646b91da91
Joven Sangha <joven.sangha@surreyschools.ca>
2023-02-07 12:54:48 -0800

    basic controller

--- Player.gd
+++ Player.gd
@@ -10,3 +10,9 @@ func _physics_process(delta):
 	else:
 		motion.y = 0
 	move_and_slide(motion)
+	if Input. is_action_pressed("ui_right"):
+		motion.x = 100
+	elif Input. is_action_pressed("ui_left"):
+		motion.x = -100
+	else:
+		motion.x = 0
