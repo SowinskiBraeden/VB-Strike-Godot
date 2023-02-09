83bf31774d4dcbcefa27a5e38939b00851daaadc
Braeden Sowinski <sowinskibraeden@gmail.com>
2023-02-09 13:23:36 -0800

    fix character layout

---
+++ Scripts/Player.gd
@@ -0,0 +1,48 @@
+extends KinematicBody2D
+
+const GRAVITY: float = 400.0
+const JUMP_STRENGTH: int = 300
+const CAN_JUMP_TIME_LIMIT: float = 1.0 # seconds we are not allowed to jump
+
+var velocity: Vector2 = Vector2()
+
+export var PlayerSpeed: int = 100
+export var Smoothness: float = 0.1
+onready var sprite = get_node("Body")
+var jump_allowed_timer: int = 0
+
+func _ready():
+	set_physics_process(true)
+	
+func _physics_process(delta):
+	velocity.y += delta * GRAVITY
+	
+	if jump_allowed_timer > 0:
+		jump_allowed_timer -= delta
+	
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+		#sprite.set_flip_h(true)
+		sprite.set_scale(Vector2(-1, 1))
+	elif Input.is_action_pressed("ui_right"):
+		direction = 1
+		#sprite.set_flip_h(false)
+		sprite.set_scale(Vector2(1, 1))
+
+	velocity.x = lerp(velocity.x, PlayerSpeed * direction, Smoothness)
+	
+	var motion = velocity * delta
+	var collision = move_and_collide(motion)
+
+	# Are we on the ground
+	if collision:
+		motion = velocity.slide(collision.normal)
+		velocity = move_and_slide(motion)
+
+		if Input.is_action_pressed("ui_up") and is_jump_allowed():
+			velocity.y -= JUMP_STRENGTH
+			jump_allowed_timer = CAN_JUMP_TIME_LIMIT
+
+func is_jump_allowed() -> bool:
+	return (jump_allowed_timer <= 0)
